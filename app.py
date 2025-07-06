7319938c6f60f4f478ea5a4ef1e4a466b4ef4bd3
David <david171612@gmail.com>
2025-07-05 22:51:21 -0400

    Agregamos un app.py y un ipynb

---
+++ app.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv('dataset/vehicles_us.csv') # leer los datos
+hist_button = st.button('Construir histograma') # crear un botón
+
+if hist_button: # si el botón es presionado
+  st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+  
+  fig = px.histogram(car_data, x="odometer")
+
+  st.plotly_chart(fig, use_container_width=True) # mostrar el histograma
